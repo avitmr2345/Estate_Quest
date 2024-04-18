17ef137a32f581dfb0e044eebaf355ca4777bb86
Avish <avitmr2345@gmail.com>
2024-04-18 15:12:19 +0530

    created update user api route

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import authRouter from "./routes/auth.route.js";
+import userRouter from "./routes/user.route.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
@@ -17,10 +19,13 @@ mongoose
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
 
-app.use((err, req, res, next) => {        //middleware for handling error
+app.use((err, req, res, next) => {
+  //middleware for handling error
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
   return res.status(statusCode).json({
